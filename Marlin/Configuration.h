1ac5cdad0e2e1eff2e33bba9b21fc6da61478850
jordan <carrierjordan@gmail.com>
2022-01-27 18:56:57 +0100

    Fix board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -101,7 +101,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_BTT_OCTOPUS_V1_1
 #endif
 
 /**
@@ -147,11 +147,6 @@
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
-
-// Choose the name from boards.h that matches your setup
-#ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_OCTOPUS_V1_1
-#endif
 
 // Name displayed in the LCD "Ready" message and Info menu
 #define CUSTOM_MACHINE_NAME "CLINSPARKS V2.0.9"
